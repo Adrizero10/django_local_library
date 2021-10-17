cd8f5e97c17611d40b4c9f29e2920ccffe8c0d99
Adrizero10 <Adrizero2001@gmail.com>
2021-10-17 19:42:10 +0200

    Procfile

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -30,11 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['http://the-yogify.herokuapp.com/',
-'https://the-yogify.herokuapp.com/',
-'the-yogify.herokuapp.com',
-'the-yogify.herokuapp.com/',
-'127.0.0.1']
+ALLOWED_HOSTS = ['locallibrary1234.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
